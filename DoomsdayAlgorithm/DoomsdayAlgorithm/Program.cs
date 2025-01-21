556158d0ba20797b969d415daf34da242d94eebd
Ace929 <65095842+Ace929@users.noreply.github.com>
2025-01-20 19:57:07 -0600

    Final Doomsday
    It works!! Let's goooooo!
    - Side note, it doesn't take into consideration partial day market closures :(

--- DoomsdayAlgorithm/DoomsdayAlgorithm/Program.cs
+++ DoomsdayAlgorithm/DoomsdayAlgorithm/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using System.Reflection.Metadata.Ecma335;
 
@@ -7,34 +8,20 @@ namespace DoomsdayAlgorithm
     class Program
     {
         static void Main(string[] args)
-        {
-            DateTime thisDate = new DateTime(2056, 4, 4);
-            int month = thisDate.Month;
-            int year = thisDate.Year - 2000;
-            int day = thisDate.Day;
+        { // Markets dont close on Veterans day and Columbus day
+            //But, they do close on good friday
+            int year = 2022;
 
-            double doomsday = 2; // Because 2000 is Tuesday
-
-
-            double x = 38; // Year
-            double y = 4;
-            double z = Math.Floor(x / y);
-
-            double w = (x + z) % 7;
-            doomsday = w + 2;
-            doomsday = doomsday % 7;
-
-            //Console.WriteLine(doomsday);
-
-            string independenceDay = IndependenceDay(doomsday);
-            //Console.WriteLine(independenceDay);
-
-            string thanksGiving = ColumbusDay(doomsday);
-            Console.WriteLine(thanksGiving);
-
+            var holidays = GetAllMarketHolidays(year);
+            
+            foreach (var item in holidays)
+            {
+                Console.WriteLine(item);
+            }
+            
         }
 
-        static string NewYearsDay(double doomsday, double year)
+        static (int date, int dow) NewYearsDay(int doomsday, int year)
         {
             bool isLeapYear = false;
             if ((year % 4) == 0)
@@ -45,40 +32,40 @@ namespace DoomsdayAlgorithm
             {
                 if (doomsday == 0 || doomsday == 1 || doomsday == 2)
                 {
-                    double nyeDOW = doomsday + 4;
+                    int nyeDOW = doomsday + 4;
                     //Console.WriteLine(nyeDOW);
                     string nyeDay = IntToDay(nyeDOW);
-                    return $"New Year's Day falls on {nyeDay}";
+                    return (1, nyeDOW);
                 }
                 else
                 {
-                    double nyeDOW = doomsday - 3;
+                    int nyeDOW = doomsday - 3;
                     //Console.WriteLine(nyeDOW);
                     string nyeDay = IntToDay(nyeDOW);
-                    return $"New Year's Day falls on {nyeDay}";
+                    return (1, nyeDOW);
                 }
             }
             else
             {
                 if (doomsday == 0 || doomsday == 1)
                 {
-                    double nyeDOW = doomsday + 5;
+                    int nyeDOW = doomsday + 5;
                     //Console.WriteLine(nyeDOW);
                     string nyeDay = IntToDay(nyeDOW);
-                    return $"New Year's  falls on {nyeDay}";
+                    return (1, nyeDOW);
                 }
                 else
                 {
-                    double nyeDOW = doomsday - 2;
+                    int nyeDOW = doomsday - 2;
                     //Console.WriteLine(nyeDOW);
                     string nyeDay = IntToDay(nyeDOW);
-                    return $"New Year's  falls on {nyeDay}";
+                    return (1, nyeDOW);
                 }
 
             }
         }
 
-        static string MLKDay(double doomsday, double year)
+        static (int date, int dow) MLKDay(int doomsday, int year)
         {
             bool isLeapYear = false;
             if ((year % 4) == 0)
@@ -87,20 +74,19 @@ namespace DoomsdayAlgorithm
             }
             if (isLeapYear == true)
             {
-                double firstDOM = (doomsday + 3) % 7;
-                double MLKDate = ((8 - firstDOM) % 7) + 14;
-                Console.WriteLine($"MLK Day is on January {MLKDate}");
+                int firstDOM = (doomsday + 3) % 7;
+                int MLKDate = ((8 - firstDOM) % 7) + 14;
+                return (MLKDate, 1);
             }
             else
             {
-                double firstDOM = (doomsday + 4) % 7;
-                double MLKDate = ((8 - firstDOM) % 7) + 14;
-                Console.WriteLine($"MLK Day is on January {MLKDate}");
+                int firstDOM = (doomsday + 4) % 7;
+                int MLKDate = ((8 - firstDOM) % 7) + 14;
+                return (MLKDate, 1);
             }
-            return "Done";
         }
 
-        static string WashingtonDay(double doomsday, double year)
+        static (int date, int dow) PresidentsDay(int doomsday, int year)
         {
             bool isLeapYear = false;
             if ((year % 4) == 0)
@@ -109,138 +95,120 @@ namespace DoomsdayAlgorithm
             }
             if (isLeapYear == true) // Doomsday is the first
             {
-                double washingtonDay = ((8 - doomsday) % 7) + 15;
-                Console.WriteLine($"President's Day is on February {washingtonDay}");
+                int washingtonDay = ((8 - doomsday) % 7) + 15;
+                return (washingtonDay, 1);
             }
             else
             {
-                double firstDOM = (doomsday + 1) % 7;
-                double washingtonDay = ((8 - firstDOM) % 7) + 15;
-                Console.WriteLine($"President's Day is on February {washingtonDay}");
+                int firstDOM = (doomsday + 1) % 7;
+                int washingtonDay = ((8 - firstDOM) % 7) + 15;
+                return (washingtonDay, 1);
             }
-            return "Done";
         }
 
-        static string MemorialDay(double doomsday)
+        static (DateTime date, int dow) GoodFriday(int year)
         {
-            double lastDay = (doomsday + 1) % 7;
-            double memDate = 31 - ((lastDay + 6) % 7);
-            return $"Memorial Day falls on May {memDate}";
+            int y = year;
+            int c = y / 100;
+            int n = y - 19 * (y / 19);
+            int k = (c - 17) / 25;
+            int i = c - c / 4 - (c - k) / 3 + 19 * n + 15;
+            i = i - 30 * (i / 30);
+            i = i - (i / 28) * (1 - (i / 28) * (29 / (i + 1)) * ((21 - n) / 11));
+            int j = y + y / 4 + i + 2 - c + c / 4;
+            j = j - 7 * (j / 7);
+            int l = i - j;
+            int month = 3 + (l + 40) / 44;
+            int day = l + 28 - 31 * (month / 4);
+            DateTime goodFriday = new DateTime(year, month, day - 2);
+            return (goodFriday,5);
         }
 
-        static string Juneteenth(double doomsday)
+        static (int date, int dow) MemorialDay(int doomsday)
         {
-            double jtDOW = (doomsday + 6) % 7;
-            return $"Juneteenth falls on {IntToDay(jtDOW)}";
+            int lastDay = (doomsday + 1) % 7;
+            int memDate = 31 - ((lastDay + 6) % 7);
+            return (memDate, 1);
         }
 
-        static string IndependenceDay(double doomsday)
+        static (int date, int dow) Juneteenth(int doomsday)
         {
-            if (doomsday == 0)
-            {
-                return "7/4 is a Sunday. Market is closed Monday 7/5";
-            }
-            else if (doomsday == 6)
-            {
-                return "7/4 is a Saturday. Market is closed Friday 7/3";
-            }
-            else if (doomsday == 5)
-            {
-                return "7/4 is a Friday. Market is closed Friday 7/4";
-            }
-            else if (doomsday == 4)
-            {
-                return "7/4 is a Thursday. Market is closed Thursday 7/4";
-            }
-            else if (doomsday == 3)
-            {
-                return "7/4 is a Wednesday. Market is closed Wednesday 7/4";
-            }
-            else if (doomsday == 2)
-            {
-                return "7/4 is a Tuesday. Market is closed Tuesday 7/4";
-            }
-            else if (doomsday == 1)
-            {
-                return "7/4 is a Monday. Market is closed Monday 7/4";
-            }
-            else
-            {
-                return "Invalid Day";
-            }
-
+            int jtDOW = (doomsday + 6) % 7;
+            return (19, jtDOW);
         }
 
-        static string LaborDay(double doomsday)
+        static (int date, int dow) IndependenceDay(int doomsday)
+        {
+            return (4, doomsday);
+        }
+
+        static (int date, int dow) LaborDay(int doomsday)
         {
             // Doomsday is the 5th
             if (doomsday == 0 || doomsday == 6)
             {
-                double laborDayDate = ((8 - doomsday) % 7) + 5;
-                return $"Labor Day falls on September {laborDayDate}";
+                int laborDayDate = ((8 - doomsday) % 7) + 5;
+                return (laborDayDate, 1);
             }
             else
             {
-                double laborDayDate = (5 - (doomsday + 6) % 7);
-                return $"Labor Day falls on September {laborDayDate}";
+                int laborDayDate = (5 - (doomsday + 6) % 7);
+                return (laborDayDate, 1);
             }
         }
 
-        static string ColumbusDay(double doomsday)
+        static (int date, int dow) ColumbusDay(int doomsday)
         {
             // October 3rd is doomsday
             if (doomsday == 0 || doomsday == 6)
             {
-                double dayDistance = (8 - doomsday) % 7;
-                double columbusDayDate = dayDistance + 10;
-                return $"Columbus Day falls on October {columbusDayDate}";
+                int dayDistance = (8 - doomsday) % 7;
+                int columbusDayDate = dayDistance + 10;
+                return (columbusDayDate, 1);
             }
             else if (doomsday == 1 || doomsday == 2 || doomsday == 3)
             {
-                double dayDistance = (doomsday + 6) % 7;
-                double columbusDayDate = (7 - dayDistance) + 3;
-                return $"Columbus Day falls on October {columbusDayDate}";
+                int dayDistance = (doomsday + 6) % 7;
+                int columbusDayDate = (7 - dayDistance) + 3;
+                return (columbusDayDate, 1);
             }
             else
             {
-                double dayDistance = (8 - doomsday) % 7;
-                double columbusDayDate = (7 + dayDistance) + 3;
-                return $"Columbus Day falls on October {columbusDayDate}";
+                int dayDistance = (8 - doomsday) % 7;
+                int columbusDayDate = (7 + dayDistance) + 3;
+                return (columbusDayDate, 1);
             }
-            return null;
         }
 
-        static string VeteransDay(double doomsday)
+        static (int date, int dow) VeteransDay(int doomsday)
         {
-            double vetDay = (doomsday + 4) % 7;
-            return $"Veterans Day falls on {IntToDay(vetDay)}";
+            int vetDay = (doomsday + 4) % 7;
+            return (11, vetDay);
 
         }
 
-        static string ThanksGiving(double doomsday)
+        static (int date, int dow) ThanksGiving(int doomsday)
         {
-            double preFirstDay = doomsday + 1;
-            double firstDay = preFirstDay % 7;
+            int preFirstDay = doomsday + 1;
+            int firstDay = preFirstDay % 7;
 
             if (firstDay == 4 || firstDay == 5 || firstDay == 6)
             {
-                double date = 32 - doomsday;
-                Console.WriteLine($"Thanksgiving falls on Nov. {date}");
+                int date = 32 - doomsday;
+                return (date, 4);
             }
-            else if (firstDay == 0 || firstDay == 1 || firstDay == 2 || firstDay == 3)
+            else
             {
-                double date = 25 - doomsday;
-                Console.WriteLine($"Thanksgiving falls on Nov. {date}");
+                int date = 25 - doomsday;
+                return (date, 4);
             }
-
-            return "Done";
         }
 
-        static string Christmas(double doomsday)
+        static (int date, int dow) Christmas(int doomsday)
         {
-            double xmas = ((doomsday + 7) - 1) % 7;
-            string xmasDOW = IntToDay(xmas);
-            return $"Christmas falls on {xmasDOW}";
+            int xmas = ((doomsday + 7) - 1) % 7;
+            //string xmasDOW = IntToDay(xmas);
+            return (25, xmas);
         }
 
         static string IntToDay(double day)
@@ -278,7 +246,138 @@ namespace DoomsdayAlgorithm
                 return "Invalid Day";
             }
         }
-    }
 
-    
+        static List<(DateTime date, int dow)> GetAllMarketHolidays(int year)
+        {
+            year = year - 2000;
+            int doomsday = 2; // Because 2000 is Tuesday (Only initial doomsday [For the century])
+            int y = 4;
+            int z = (int)Math.Floor((double)year / y);
+            int w = (int)(year + z) % 7;
+            doomsday = w + 2;
+            doomsday = doomsday % 7;
+
+            // This is for next year, which we will use for next year's New Years Day
+            int _year = year + 1;
+            int _doomsday = 2;
+            int _z = (int)Math.Floor((double)_year / y);
+            int _w = (int)(_year + _z) % 7;
+            _doomsday = _w + 2;
+            _doomsday = _doomsday % 7;
+
+            int formattedYear = year + 2000;
+
+            var nye = NewYearsDay(doomsday, year); // can be weekend (SOLVED)
+            var nyePlusOne = NewYearsDay(_doomsday, _year);
+            var mlk = MLKDay(doomsday, year);
+            var pres = PresidentsDay(doomsday, year);
+            var good = GoodFriday(formattedYear);
+            var mem = MemorialDay(doomsday);
+            var jun = Juneteenth(doomsday); // can be weekend (SOLVED)
+            var ind = IndependenceDay(doomsday); // can be weekend (SOLVED)
+            var lab = LaborDay(doomsday);
+            var col = ColumbusDay(doomsday);
+            var vet = VeteransDay(doomsday); // can be weekend (SOLVED)
+            var thank = ThanksGiving(doomsday);
+            var xmas = Christmas(doomsday); // can be weekend (SOLVED)
+            //Console.WriteLine(xmas.Item1);
+
+            List<(DateTime date, int dow)> finalClosures = new List<(DateTime date, int dow)>();
+
+            // Check new years day
+            if (nyePlusOne.dow == 6)
+            {
+                DateTime newYearsPlusOne = new DateTime(formattedYear, 12, 31);
+                finalClosures.Add((newYearsPlusOne, 5));
+            }
+            else if (nye.dow == 0)
+            {
+                DateTime newYearsDay = new DateTime(formattedYear, 1, 2);
+                finalClosures.Add((newYearsDay, 1));
+            }
+            else if (nye.dow != 6)
+            {
+                DateTime newYearsDay = new DateTime(formattedYear, 1, 1);
+                finalClosures.Add((newYearsDay, nye.dow));
+            }
+
+            finalClosures.Add((new DateTime(formattedYear, 1, mlk.date), 1)); // Add MLK Day
+            finalClosures.Add((new DateTime(formattedYear, 2, pres.date), 1)); // Add Presidents Day
+            finalClosures.Add((good.date, good.dow)); // Add Good Friday
+            finalClosures.Add((new DateTime(formattedYear, 5, mem.date), 1)); // Add Memorial Day
+
+            if (jun.dow == 0)
+            {
+                DateTime juneteenth = new DateTime(formattedYear, 6, 20);
+                finalClosures.Add((juneteenth, 1));
+            }
+            else if (jun.dow == 6)
+            {
+                DateTime juneteenth = new DateTime(formattedYear, 6, 18);
+                finalClosures.Add((juneteenth, 5));
+            }
+            else
+            {
+                DateTime juneteenth = new DateTime(formattedYear, 6, 19);
+                finalClosures.Add((juneteenth, jun.dow));
+            }
+            if (ind.dow == 0)
+            {
+                DateTime independenceDay = new DateTime(formattedYear, 7, 5);
+                finalClosures.Add((independenceDay, 1));
+            }
+            else if (ind.dow == 6)
+            {
+                DateTime independenceDay = new DateTime(formattedYear, 7, 3);
+                finalClosures.Add((independenceDay, 5));
+            }
+            else
+            {
+                DateTime independenceDay = new DateTime(formattedYear, 7, 4);
+                finalClosures.Add((independenceDay, ind.dow));
+            }
+
+            finalClosures.Add((new DateTime(formattedYear, 9, lab.date), 1)); // Add Labor Day
+            //finalClosures.Add((new DateTime(formattedYear, 10, col.date), 1)); // Add Columbus Day
+
+            // This is Veteran's Day
+            /*
+            if (vet.dow == 0)
+            {
+                DateTime veteransDay = new DateTime(formattedYear, 11, 12);
+                finalClosures.Add((veteransDay, 1));
+            }
+            else if (vet.dow == 6)
+            {
+                DateTime veteransDay = new DateTime(formattedYear, 11, 10);
+                finalClosures.Add((veteransDay, 5));
+            }
+            else
+            {
+                DateTime veteransDay = new DateTime(formattedYear, 11, 11);
+                finalClosures.Add((veteransDay, vet.dow));
+            }
+            */
+
+            finalClosures.Add((new DateTime(formattedYear, 11, thank.date), 4)); // Add Thanksgiving
+
+            if (xmas.dow == 0)
+            {
+                DateTime christmas = new DateTime(formattedYear, 12, 26);
+                finalClosures.Add((christmas, 1));
+            }
+            else if (xmas.dow == 6)
+            {
+                DateTime christmas = new DateTime(formattedYear, 12, 24);
+                finalClosures.Add((christmas, 5));
+            }
+            else
+            {
+                DateTime christmas = new DateTime(formattedYear, 12, 25);
+                finalClosures.Add((christmas, xmas.dow));
+            }
+
+            return finalClosures; // This is a list of market closures because of holidays
+        }
+    }
 }
